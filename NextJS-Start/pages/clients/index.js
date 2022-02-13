62b7c0a63c818ff0889bed8c77359c4fbf46a50b
fearm3 <ibraaa1628@gmail.com>
2022-02-13 15:16:29 +0300

    add Navigate to Dynamic Routes

--- NextJS-Start/pages/clients/index.js
+++ NextJS-Start/pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    {
+      id: "apple",
+      name: "Apple",
+    },
+    {
+      id: "straw",
+      name: "Strawberry",
+    },
+  ];
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => {
+          const { id, name } = client;
+          return (
+            <li key={id}>
+              <Link href={`/clients/${id}`}>{name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
