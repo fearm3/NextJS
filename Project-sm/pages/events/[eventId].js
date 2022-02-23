044a7c23c5bbb7d075b1904c9794ca8508f8622c
fearm3 <ibraaa1628@gmail.com>
2022-02-24 01:21:40 +0300

    add Head and meta tags

--- Project-sm/pages/events/[eventId].js
+++ Project-sm/pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import Head from "next/head";
 // import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage(props) {
@@ -18,6 +19,10 @@ function EventDetailPage(props) {
   }
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
