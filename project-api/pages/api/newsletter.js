68ef340a0a89005652356f4addd9f6876cef9419
fearm3 <ibraaa1628@gmail.com>
2022-02-25 22:40:55 +0300

    add MongodB

--- project-api/pages/api/newsletter.js
+++ project-api/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,8 +8,14 @@ function handler(req, res) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+    const client = await MongoClient.connect(
+      "mongodb+srv://fearm3:123456Pp@cluster0.q0r77.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
 
-    console.log(userEmail);
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
     res.status(201).json({ message: "Signed up" });
   }
 }
